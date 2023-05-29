f0ad867a497b341e816e5372ef7d373fab0d3e6e
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2023-05-29 16:52:22 -0700

    small changes

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import datetime
 
 def pomodoro():
     total_time = 25 #* 60
+    rest_time = 5
     print((f"Time remaining: {total_time}"))
+    s = input("Enter any character to start: ")
     while total_time > 0:
         total_time -= 1
         time.sleep(1)
-    print("Time is up!")
-
+    print("Time is up! Time for a break!")
+    s = input("Enter any character to start: ")
+    print((f"Time remaining: {rest_time}"))
+    while rest_time > 0:
+        rest_time -= 1
+        time.sleep(1)
 
 pomodoro()
